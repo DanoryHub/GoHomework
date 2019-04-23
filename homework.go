7f7c96cc5e1db9d3d2e4ef7695414e79c555ae78
Danory <danorywork@gmail.com>
2019-04-23 23:11:12 +0300

    delete some files, prepare to create repo

--- homework.go
+++ homework.go
@@ -5,8 +5,10 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	_ "log"
+	"math/rand"
 	_ "math/rand"
 	"net/http"
+	"strconv"
 	_ "strconv"
 )
 
@@ -26,7 +28,17 @@ type Account struct{
 	Currency string `json:"currency"`
 }
 
+
 var users []User
+
+type UserRepository interface {
+	GetAll() ([]User, error)
+	FindByID(id string) (User, error)
+	UpdateUser(id string, user User) (User, error)
+
+}
+
+var storage UserRepository
 
 func getUsers(writer http.ResponseWriter, request *http.Request){
 	writer.Header().Set("Content-Type", "application/json")
@@ -36,8 +48,9 @@ func getUsers(writer http.ResponseWriter, request *http.Request){
 func getUser(writer http.ResponseWriter, request *http.Request){
 	writer.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(request)
+
 	for _, item := range users{
-		if item.ID == params["id"]{
+		if item.ID == params["user_id"]{
 			_ = json.NewEncoder(writer).Encode(item)
 			return
 		}
@@ -84,8 +97,85 @@ func deleteUser(writer http.ResponseWriter, request *http.Request){
 	_ = json.NewEncoder(writer).Encode(users)
 }
 
+func deleteUserAccount(writer http.ResponseWriter, request *http.Request){
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for usr_index, user := range users{
+		if user.ID == params["user_id"]{
+			for index, account := range user.Accounts{
+				if account.ID == params["account_id"]{
+					users[usr_index].Accounts = append(users[usr_index].Accounts[:index],users[usr_index].Accounts[index+1:]...)
+					break
+				}
+			}
+			break
+		}
+	}
+	_ = json.NewEncoder(writer).Encode(users)
+}
+
+func createUser(writer http.ResponseWriter, request *http.Request){
+	writer.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	user.ID = strconv.Itoa(rand.Intn(10000000000))
+	users = append(users, user)
+	_ = json.NewEncoder(writer).Encode(user)
+}
+
+func createUserAccount(writer http.ResponseWriter, request *http.Request){
+	writer.Header().Set("Content-Type", "application/json")
+	var account Account
+	params := mux.Vars(request)
+	_ = json.NewDecoder(request.Body).Decode(&account)
+	for index, user := range users{
+		if user.ID == params["user_id"]{
+			users[index].Accounts = append(users[index].Accounts, account)
+			break
+		}
+	}
+	_ = json.NewEncoder(writer).Encode(account)
+}
+
+func updateUser(writer http.ResponseWriter, request *http.Request){
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for index, item := range users{
+		if item.ID == params["user_id"]{
+			users = append(users[:index], users[index+1:]...)
+			var user User
+			_ = json.NewDecoder(request.Body).Decode(&user)
+			user.ID = params["user_id"]
+			users = append(users, user)
+			_ = json.NewEncoder(writer).Encode(user)
+			return
+		}
+	}
+	_ = json.NewEncoder(writer).Encode(users)
+}
+
+func updateUserAccount(writer http.ResponseWriter, request *http.Request){
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for usr_index, item := range users{
+		if item.ID == params["user_id"]{
+			for index, account := range item.Accounts{
+				if account.ID == params["account_id"]{
+					users[usr_index].Accounts = append(users[usr_index].Accounts[:index], users[usr_index].Accounts[index+1:]...)
+					var account Account
+					_ = json.NewDecoder(request.Body).Decode(&account)
+					account.ID = params["account_id"]
+					users[usr_index].Accounts = append(users[usr_index].Accounts, account)
+					_ = json.NewEncoder(writer).Encode(account)
+					return
+				}
+			}
+		}
+	}
+	_ = json.NewEncoder(writer).Encode(users)
+}
+
 func main(){
-	router := mux.NewRouter()
 	account1 := Account{ID:"1",UserID: "1",Balance:"3800",Currency:"dollars"}
 	account2 := Account{ID:"2",UserID: "1",Balance:"3900",Currency:"dollars"}
 	users = append(users, User{ID:"1",Firstname:"Ivan",Lastname:"Metelsky",Email:"",SMS:""})
@@ -94,10 +184,16 @@ func main(){
 	users[0].Accounts = append(users[0].Accounts, account2)
 	users[1].Accounts = append(users[1].Accounts, account1)
 	users[1].Accounts = append(users[1].Accounts, account2)
-	router.HandleFunc("/users", getUsers).Methods("Get")
-	router.HandleFunc("/users/{id}", getUser).Methods("Get")
-	router.HandleFunc("/users/{user_id}/accounts", getUserAccounts).Methods("Get")
-	router.HandleFunc("/users/{user_id}/accounts/{account_id}", getUserAccount).Methods("Get")
-	router.HandleFunc("/users/{user_id}", deleteUser).Methods("Delete")
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/users/{user_id}", getUser).Methods("GET")
+	router.HandleFunc("/users/{user_id}", deleteUser).Methods("DELETE")
+	router.HandleFunc("/users/{user_id}", updateUser).Methods("PUT")
+	router.HandleFunc("/users/{user_id}/accounts", getUserAccounts).Methods("GET")
+	router.HandleFunc("/users/{user_id}/accounts", createUserAccount).Methods("POST")
+	router.HandleFunc("/users/{user_id}/accounts/{account_id}", getUserAccount).Methods("GET")
+	router.HandleFunc("/users/{user_id}/accounts/{account_id}", deleteUserAccount).Methods("DELETE")
+	router.HandleFunc("/users/{user_id}/accounts/{account_id}", updateUserAccount).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
